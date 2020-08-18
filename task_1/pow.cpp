8b1a6f9bc553d657f04421671d906ae4b2fdcdc7
her0elt <her0elt@gmail.com>
2020-08-18 14:15:22 +0200

    rename

--- task_1/pow.cpp
+++ task_1/pow.cpp
@@ -2,25 +2,25 @@
 #include  <tgmath.h>
 
 //complexity 0(n)
-double desiAlgo(double x, int n){
+double algo(double x, int n){
     if(n==0) return 1;
-    else if(n>0) return x*desiAlgo(x,(n-1));
+    else if(n>0) return x*algo(x,(n-1));
     return 0;
 }
-//complexity is log2(n)
-double desiAlgo2(double x, int n){
+//complexity is log(n)
+double algo2(double x, int n){
     if(n==0) return 1;
-    else if(n%2 == 1) return x*desiAlgo2(x*x,(n-1)/2);
-    else if(n%2 == 0) return desiAlgo2(x*x,n/2);
+    else if(n%2 == 1) return x*algo2(x*x,(n-1)/2);
+    else if(n%2 == 0) return algo2(x*x,n/2);
     return 0;
 }
-//complexity log2(n)
+//complexity log(n)
 double cppPow(double x, int n){
     return pow(x,n);
 }
 int main() {
-    printf("task 1 %f\n", desiAlgo(3,3));
-    printf("task 2 %f\n", desiAlgo2(3,3));
+    printf("task 1 %f\n", algo(3,3));
+    printf("task 2 %f\n", algo2(3,3));
     printf("task 3 %f\n", cppPow(3,3));
 
 }
