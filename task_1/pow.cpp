c7b096b998c798e7dad297d79026805892a5371d
her0elt <her0elt@gmail.com>
2020-08-20 15:31:30 +0200

    comments

--- task_1/pow.cpp
+++ task_1/pow.cpp
@@ -20,14 +20,13 @@ double algo(double x, int n){
     else if(n>0) return x*algo(x,(n-1));
     return 0;
 }
-//complexity is log(n)
+//complexity is O(log2(n))
 double algo2(double x, int n){
     if(n==0) return 1;
     else if(n%2 == 1) return x*algo2(x*x,(n-1)/2);
     else if(n%2 == 0) return algo2(x*x,n/2);
     return 0;
 }
-//complexity log(n)
 double cppPow(double x, int n){
     return pow(x,n);
 }
