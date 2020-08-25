53fc09fd8845d47eb635d713bdba3e726b3c9090
her0elt <her0elt@gmail.com>
2020-08-25 18:38:11 +0200

    fixing task 1

--- task_1/pow.cpp
+++ task_1/pow.cpp
@@ -7,13 +7,24 @@
 
 int seconds = 1;
 
-//complexity 0(n)
+//time complexity 0(n)
+//This complexity comes from computing the algorithme, 
+//which reduces my 1 every recursion which gives us a time complexity of O(n)
 double algo(double x, int n){
     if(n==0) return 1;
     else if(n>0) return x*algo(x,(n-1));
     return 0;
 }
-//complexity is O(log2(n))
+
+//time complexity is O(log(n))
+//This time complexity comes from using the mastermethod
+//T(n) = aT(n/b)+cn^k 
+//where in this case a = 1 because the recursion is called once every round
+//b = 2 because n is diveded by 2 every recursion
+//the last part of the metode cn^k = 1 because k = 0, which comes from the fact that
+//there are no loops for each recursion of the algorithme 
+//this gives us T(n/2) which when computing the mastermethod 
+// we get that b^k = a because 2^0 = 1 which means that teath == n^k log n or log(n)
 double algo2(double x, int n){
     if(n==0) return 1;
     else if(n%2 == 1) return x*algo2(x*x,(n-1)/2);
@@ -31,16 +42,19 @@ void testTime(double x, int n, std::function<double (double,int)> func){
     }
     double nano_sec = 1000000000*(seconds/count);
     double count_sec = count/seconds;
-    printf("n = %d %f times per second and %f nanoseconds pr time\n",n, count_sec,nano_sec);
-   
-   
-    
+    printf("n = %d %f times per second and %f nanoseconds pr time\n",n, count_sec,nano_sec);    
 }
+
 int main() {
     int x = 2;
+
     printf("answer algo  for 2, 10 %f\n", algo(2,10));
     printf("answer algo2 for 2, 10 %f\n", algo2(2,10));
     printf("answer pow for 2, 10 %f\n", pow(2,10));
+
+    printf("\nanswer algo  for 3, 14 %f\n", algo(3,14));
+    printf("answer algo2 for 3, 14 %f\n", algo2(3,14));
+    printf("answer pow for 3, 14 %f\n", pow(3,14));
 
 
     printf("\nalgo time complexity O(n)\n");
@@ -49,7 +63,7 @@ int main() {
     testTime(x,1000, algo); 
     testTime(x,100000, algo);
 
-    printf("\nalgo2 time complexity O(log2(n))\n");
+    printf("\nalgo2 time complexity O(log(n))\n");
     testTime(x,10, algo2);
     testTime(x,100, algo2);
     testTime(x,1000, algo2); 
